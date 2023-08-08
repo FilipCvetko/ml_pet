a77d1209949c2aae85f5fc0c4bf522233245d53f
FilipCvetko <filipcvetko123@gmail.com>
2023-08-08 23:17:10 +0200

    added custom BCE loss

--- src/ml_pet/data/utils.py
+++ src/ml_pet/data/utils.py
@@ -3,6 +3,8 @@ import re
 
 import numpy as np
 import pydicom
+import torch
+import torch.nn as nn
 
 
 def get_largest_wb_pet_thyroid_slice_number(
@@ -54,3 +56,25 @@ def interpolate_ct_dict(my_dict, key):
     )
     t = (key - left_key) / (right_key - left_key)
     return my_dict[left_key] * (1 - t) + my_dict[right_key] * t
+
+class CustomBCELoss(nn.Module):
+    def __init__(self, weight=None):
+        super(CustomBCELoss, self).__init__()
+        self.weight = weight
+
+    def forward(self, x, target):   
+        # Add epsilon to avoid taking the logarithm of zero
+        epsilon = 1e-4
+        x = torch.clamp(x, epsilon, 1 - epsilon)
+        
+        # Calculate the element-wise loss
+        loss = -((1 - target) * torch.log(1 - x) + target * torch.log(x))
+        
+        # Apply class weighting
+        if self.weight is not None:
+            loss = loss * (target * self.weight + (1 - target))
+            
+        # Compute the mean loss
+        loss = loss.mean()
+        
+        return loss
